caac467d229b2e12f88a2b6eebaf7562d3e6ff57
bensonc27 <bensonc27@nycstudents.net>
2024-11-15 12:30:50 -0500

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+async function getData() {
+  //fetch returns a promise
+  try {
+    const response = await fetch("https://api.clashroyale.com/v1/pekka");
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("sorry could not find that ");
+  }
+}
+getData();
