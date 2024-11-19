09e3adf0274832efc3f5cc08d91b5e257fc700ed
bensonc27 <bensonc27@nycstudents.net>
2024-11-19 12:29:58 -0500

    Create Cards

--- app/JS/main.js
+++ app/JS/main.js
@@ -2,13 +2,13 @@ import "../CSS/style.css";
 
 async function getData() {
   try {
-    const response = await fetch("https://mhw-db.com/monsters/16");
+    const response = await fetch("https://mhw-db.com/monsters/2");
 
     if (response.status != 200) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      document.querySelector("h1").textContent = data.name;
+      return data;
     }
   } catch (error) {
     console.log(error);
@@ -16,3 +16,34 @@ async function getData() {
   }
 }
 getData();
+
+const DomSelectors = {
+  start: document.querySelector(".start"),
+  container: document.querySelector(".container"),
+};
+
+function createCard(array) {
+  DomSelectors.container.innerHTML = "";
+
+  array.forEach((monster) => {
+    const CardHTML = `<div class = card>
+        <h1 class = "name"> Monster: ${monster.name} </h1>
+        <img class = "picture" src="${monster.image_url}">
+        <h2 class = "species"> Species: ${monster.species} </h2>
+        <h2 class = "type"> Type: ${monster.type} </h2>
+        <h2 class = "elements> Elements: ${monster.elements} </h2>
+        <p class = "resistances"> Resistance: ${monster.resistances} </p>
+        <p class = "weakness"> Weakness: ${monster.weaknesses} </p>
+        <p class = "location"> Locations: ${monster.locations} </p>
+        <p class = "disease"> Ailments: ${monster.ailments} </p>
+        <p class = "reward"> Drops: ${monster.rewards} </p>
+        <p class = "desc"> ${monster.description} </p>
+        </div>`;
+    DomSelectors.container.insertAdjacentHTML("beforeend", CardHTML);
+  });
+}
+
+start.addEventListener("click", function () {
+  const monster = data;
+  createCard(monster);
+});
