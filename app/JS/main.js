6223da76225dc434dfb1c2fea2fdb28d3e1a4a5a
bensonc27 <bensonc27@nycstudents.net>
2024-11-29 23:19:12 -0500

    tailwind

--- app/JS/main.js
+++ app/JS/main.js
@@ -9,7 +9,7 @@ async function getData() {
     } else {
       const data = await response.json();
       console.log(data);
-      createCards(data.movie);
+      createCards(data);
     }
   } catch (error) {
     console.log(error);
@@ -27,15 +27,25 @@ function createCards(data) {
   DomSelectors.container.innerHTML = "";
 
   data.forEach((movie) => {
-    const CardHTML = `<div class = card>
-        <h1 class = "title"> Title: ${movie.name} </h1>
-        <h2 class = "otitle"> Original Title: ${movie.original_title} </h2>
-        <img class="image" src="${movie.image_url}">
-        <p class = "producer"> Producer: ${movie.producer} </p>
-        <p class = "date"> Release Date: ${movie.release_date} </p>
-        <p class = "occupation"> Occupation: ${character.personal.occupation} </p>
-        <p class = "desc"> Description: ${movie.description} </p>
-        </div>`;
+    const CardHTML = `
+  <div class="card bg-base-100 w-4/12 h-5/12 shadow-xl mr-20 mt-8">
+  <figure>
+    <img class= "image "
+      src="${movie.image}"
+      >
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title "> Title: ${movie.title}</h2>
+    <h2 class="otitle "> Original Title: ${movie.original_title} </h2>
+    <h3 class="date "> Release Date: ${movie.release_date} </h3>
+        <p class="producer "> Producer: ${movie.producer} </p>
+    <p class="desc "> Description: ${movie.description} </p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Watch Now</button>
+    </div>
+  </div></div>
+     
+`;
     DomSelectors.container.insertAdjacentHTML("beforeend", CardHTML);
   });
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./main.js", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./JS/*",
+    "./main.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
