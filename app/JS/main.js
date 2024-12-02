2142ffde6e16d7010b1044be80532eddca2c8a03
bensonc27 <bensonc27@nycstudents.net>
2024-12-01 22:43:57 -0500

    Final

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,5 +1,11 @@
 import "../CSS/style.css";
 
+const DomSelectors = {
+  container: document.querySelector(".container"),
+  search: document.getElementById("search"),
+  button: document.getElementById("button"),
+};
+
 async function getData() {
   try {
     const response = await fetch("https://ghibliapi.vercel.app/films");
@@ -19,27 +25,57 @@ async function getData() {
 
 getData();
 
-const DomSelectors = {
-  container: document.querySelector(".container"),
-};
+async function filtData() {
+  try {
+    const response = await fetch("https://ghibliapi.vercel.app/films");
+
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      filterNames(data);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("sorry could not find that");
+  }
+}
+
+filtData();
+
+function filterNames(movies) {
+  DomSelectors.button.addEventListener("click", function (event) {
+    event.preventDefault();
+    const name = movies.filter(
+      (movie) => movie.title == DomSelectors.search.value
+    );
+    createCards(name);
+    reset;
+  });
+}
+
+function reset() {
+  DomSelectors.search.value = "";
+}
 
 function createCards(data) {
   DomSelectors.container.innerHTML = "";
 
   data.forEach((movie) => {
     const CardHTML = `
-  <div class="card bg-base-100 w-4/12 h-5/12 shadow-xl mr-20 mt-8">
+  <div class="card bg-amber-100 bg-base-100 w-3/12 lg:w-1/10 md:w-3/10 sm:w-6/10 shadow-xl ml-8 mt-8">
   <figure>
     <img class= "image "
-      src="${movie.image}"
+      src="${movie.image}" alt="${movie.title} Picture"
       >
   </figure>
   <div class="card-body">
-    <h2 class="card-title "> Title: ${movie.title}</h2>
+    <h1 class="card-title "> Title: ${movie.title}</h1>
     <h2 class="otitle "> Original Title: ${movie.original_title} </h2>
     <h3 class="date "> Release Date: ${movie.release_date} </h3>
-        <p class="producer "> Producer: ${movie.producer} </p>
-    <p class="desc "> Description: ${movie.description} </p>
+        <h3 class="producer "> Producer: ${movie.producer} </h3>
+    <p class="desc text-xs"> Description: ${movie.description} </p>
     <div class="card-actions justify-end">
       <button class="btn btn-primary">Watch Now</button>
     </div>
